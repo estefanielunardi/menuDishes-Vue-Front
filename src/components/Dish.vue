3a9d906479f833e5b6f5d6f8e3e2664ecac1f5ad
Eni <estefanielunardi@users.noreply.github.com>
2021-02-12 13:05:27 +0100

    Dish: component Dish - Api

---
+++ src/components/Dish.vue
@@ -0,0 +1,33 @@
+<template>
+    <tr>
+        <td>{{ dish.id }}</td>
+        <td>{{ dish.image }}</td>
+        <td>{{ dish.plate }}</td>
+        <td>{{ dish.price }}</td>
+        <td>{{ dish.description }}</td>
+        <td>
+            <button class="mini ui blue button">Edit</button>
+            <button class="mini ui red button" @click="onDelete">Delete</button>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: 'Dish',
+    props: {
+        dish: {
+            type: Object
+        }
+    },
+    methods: {
+        onDelete() {
+            // console.log("Dish Delete" + this.dish.id);
+            this.$emit("onDelete" , this.dish.id);
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
